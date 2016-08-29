5fc64082af5958faf5566ff154fce3db239f4712
Marco Berardini <mberardini@evolution-man.com>
2016-08-29 09:41:57 -0700

    [client] Created favorite component and imported it to app component

--- app/app.component.js
+++ app/app.component.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', "./courses.component", "./authors.component"], function(exports_1, context_1) {
+System.register(['angular2/core', "./courses.component", "./authors.component", "./favorite.component"], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,7 +10,7 @@ System.register(['angular2/core', "./courses.component", "./authors.component"],
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, courses_component_1, authors_component_1;
+    var core_1, courses_component_1, authors_component_1, favorite_component_1;
     var AppComponent;
     return {
         setters:[
@@ -22,16 +22,26 @@ System.register(['angular2/core', "./courses.component", "./authors.component"],
             },
             function (authors_component_1_1) {
                 authors_component_1 = authors_component_1_1;
+            },
+            function (favorite_component_1_1) {
+                favorite_component_1 = favorite_component_1_1;
             }],
         execute: function() {
             AppComponent = (function () {
                 function AppComponent() {
+                    this.post = {
+                        title: "Title",
+                        isFavorite: true
+                    };
                 }
+                AppComponent.prototype.onFavoriteChange = function ($event) {
+                    console.log($event);
+                };
                 AppComponent = __decorate([
                     core_1.Component({
                         selector: 'my-app',
-                        template: "<h1>My First Angular 2 App</h1>\n                <my-courses></my-courses>\n                <my-author></my-author>\n             ",
-                        directives: [courses_component_1.CourseComponent, authors_component_1.AuthorComponent]
+                        template: " <div class=\"container\">\n                    <h1>My First Angular 2 App</h1>\n                    <my-courses></my-courses>\n                    <my-author></my-author>\n                    <my-favorite \n                    [is-favorite]=\"post.isFavorite\"\n                    (change)=\"onFavoriteChange($event)\"\n                    ></my-favorite>\n                </div>\n             ",
+                        directives: [courses_component_1.CourseComponent, authors_component_1.AuthorComponent, favorite_component_1.FavoriteComponent]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], AppComponent);
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,31 @@
 import { Component } from 'angular2/core';
 import { CourseComponent } from "./courses.component";
 import { AuthorComponent } from "./authors.component";
+import {FavoriteComponent} from "./favorite.component";
+
 
 
 @Component({
     selector: 'my-app',
-    template: `<h1>My First Angular 2 App</h1>
-                <my-courses></my-courses>
-                <my-author></my-author>
+    template: ` <div class="container">
+                    <h1>My First Angular 2 App</h1>
+                    <my-courses></my-courses>
+                    <my-author></my-author>
+                    <my-favorite 
+                    [is-favorite]="post.isFavorite"
+                    (change)="onFavoriteChange($event)"
+                    ></my-favorite>
+                </div>
              `,
-    directives: [CourseComponent, AuthorComponent]
+    directives: [CourseComponent, AuthorComponent, FavoriteComponent]
 })
-export class AppComponent { }
+export class AppComponent {
+    post = {
+        title: "Title",
+        isFavorite: true
+    }
+
+    onFavoriteChange($event){
+        console.log($event);
+    }
+}
